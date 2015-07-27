a278791803d9507be8d53dd89e22159878c2290a
Nazgul <guobing_xu@163.com>
2015-07-27 17:20:30 +0800

    上传正式的一个GIT版本
    提示：写代码要小心

---
+++ Application/Admin/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'.
+    'MODULE_ALLOW_LIST' =>    array('Home','Admin',),
+    'TMPL_L_DELIM'          =>  '<{',            // 模板引擎普通标签开始标记
+    'TMPL_R_DELIM'          =>  '}>',            // 模板引擎普通标签结束标记
+    //主题静态文件路径
+    'TMPL_PARSE_STRING' => array(
+        '__STATIC__' => __ROOT__.'/Application/'.MODULE_NAME.'/View/' . '/Public/static',),
+);
